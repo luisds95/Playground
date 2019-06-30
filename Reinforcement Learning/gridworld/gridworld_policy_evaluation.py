539fe9e0525e8a1c5f6f1d7628b20e8aad7303ef
Luis Da Silva <luisdanield@gmail.com>
2019-06-30 23:12:24 +0100

    Jack's rental car problem added

--- Reinforcement Learning/gridworld/gridworld_policy_evaluation.py
+++ Reinforcement Learning/gridworld/gridworld_policy_evaluation.py
@@ -1,6 +1,7 @@
 '''
 June 06, 2019.
 Luis Da Silva.
+luisdanield@gmail.com
 
 This file evaluates a gridworld problem that is solved under a random action policy.
 The outcome is a state value matrix for each of the possible states.
@@ -8,37 +9,42 @@ The outcome is a state value matrix for each of the possible states.
 
 import numpy as np
 from itertools import product
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-WORLD_SIZE = 5
 ACTIONS = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 ACTION_PROBA = 1 / len(ACTIONS)
 
 
-def is_terminal(x, y):
-    return (x == 0 and y == 0) or (x == WORLD_SIZE-1 and y == WORLD_SIZE-1)
+def is_terminal(x, y, world_size):
+    """Identify whether a state is terminal or not (top-left and bottom-right corner)"""
+    return (x == 0 and y == 0) or (x == world_size-1 and y == world_size-1)
 
 
-def make_action(state, action, reward=-1):
+def make_action(state, action, world_size, reward=-1):
     # Returns: state, reward and whether the action was legal
     new_state = np.sum((state, action), axis=0)
-    if new_state.min() < 0 or new_state.max() >= WORLD_SIZE:
+    if new_state.min() < 0 or new_state.max() >= world_size:
         return None, 0, False
     else:
         return new_state.tolist(), reward, True
 
 
 def policy_greedy(state_value_per_action):
-    argmax = np.argmax(state_value_per_action)
-    return state_value_per_action[argmax]
+    """Returns the greediest action"""
+    return np.max(state_value_per_action)
 
 
 def policy_random(state_value_per_action):
-    return np.sum(state_value_per_action) / len(state_value_per_action)
+    """If actions are taken at random, then the value of a state is its mean"""
+    return np.mean(state_value_per_action)
 
 
-def iterative_state_evaluation(gamma=1, policy='greedy', verbose=10):
+def iterative_state_evaluation(gamma=1, policy='greedy', world_size=5, verbose=10, max_iter=1000):
     if gamma < 0 or gamma > 1:
         raise ValueError('gamma must be between 0 and 1.')
+
+    # Select policy
     if policy.lower() == 'greedy':
         policy_func = policy_greedy
     elif policy.lower() == 'random':
@@ -49,14 +55,15 @@ def iterative_state_evaluation(gamma=1, policy='greedy', verbose=10):
         raise NotImplementedError('Policy "{}" has not been implemented.'.format(policy))
     print('Running "{}" policy.'.format(policy.title()))
 
-    state_values = np.zeros((WORLD_SIZE, WORLD_SIZE))
+    state_values = np.zeros((world_size, world_size))
 
     iter = 0
-    while True:
+    # Loop until the values converge or the maximum of iterations has been reached
+    while iter < max_iter:
         old_state_values = state_values.copy()
         # For each of the possible states
-        for x, y in product(range(WORLD_SIZE), range(WORLD_SIZE)):
-            if is_terminal(x, y):
+        for x, y in product(range(world_size), range(world_size)):
+            if is_terminal(x, y, world_size):
                 # No evaluation here
                 continue
 
@@ -64,7 +71,7 @@ def iterative_state_evaluation(gamma=1, policy='greedy', verbose=10):
             state_value = []
             for action in ACTIONS:
                 # Get the reward for the action and the new state
-                next_state, r, legal = make_action((x, y), action)
+                next_state, r, legal = make_action((x, y), action, world_size)
                 if legal:
                     next_x, next_y = next_state
                     # Save the value
@@ -73,9 +80,11 @@ def iterative_state_evaluation(gamma=1, policy='greedy', verbose=10):
             # Action probability may be included outside the loop because this is a equiprobable problem
             state_values[x, y] = policy_func(state_value)
 
-        # If there were no changes, stop the loop
+        # Calculate changes
         delta = np.sum(np.abs(state_values - old_state_values))
         iter += 1
+
+        # If there were no changes, stop the loop
         if delta <= 1e-3:
             print('Iter: {}. Delta: {:.2f}'.format(iter, delta))
             break
@@ -86,4 +95,9 @@ def iterative_state_evaluation(gamma=1, policy='greedy', verbose=10):
 
 
 if __name__ == '__main__':
-    print(iterative_state_evaluation(gamma=0.9, policy='random').round(2))
+    state_values = iterative_state_evaluation(gamma=0.9, policy='greedy', world_size=5, verbose=1)
+
+    sns.heatmap(state_values, cmap='bwr_r', annot=True, fmt='.2f')
+    plt.title('State Values')
+    plt.savefig('graphs/gridworld_greedy_policy_state_values.png')
+    plt.show()
---
+++ Reinforcement Learning/gridworld/jacks_rental.py
@@ -0,0 +1,200 @@
+"""
+June 29, 2019.
+Luis Da Silva.
+luisdanield@gmail.com
+
+As described in Sutton's book, page 81. Jack has a car rental with two locations,
+he receives an amount d for every rented car. If there are no cars available then he
+receives nothing. He may have up to n cars in stock between both locations. m cars may
+be moved between locations each night. Rentals and requests follow a poisson distribution.
+The idea is to maximize jack's profit.
+"""
+import numpy as np
+from scipy.stats import poisson
+from itertools import product
+import matplotlib.pyplot as plt
+import seaborn as sns
+from tqdm import tqdm
+
+RENTAL_PRICE = 10
+MOVE_COST = 2
+STOCK_LIMIT = 20
+MOVE_LIMIT = 5
+REQUEST_LAMBDAS = (3, 4)
+RETURN_LAMBDAS = (3, 2)
+DISCOUNT_RATE = 0.9
+TOLERANCE = 1e-1
+POISSON_TOLERANCE = 1e-4
+COST_STORAGE = 4
+
+
+def get_poisson_dict(l, ns):
+    """
+    Calculate probabilities from a Poisson distribution.
+    :param l: int. Expected number.
+    :param ns: int. Max consecutive possibility
+    :return: dictionary with {number: probability} for each n from 0 to ns.
+    """
+    total = 0
+    d = {}
+
+    # Calculate poisson probabilities
+    for n in range(ns+1):
+        proba = poisson.pmf(n, l)
+
+        # Speed up computation by smoothing small values
+        if proba >= POISSON_TOLERANCE:
+            total += proba
+            d[n] = proba
+
+    # Normalise probabilities
+    for k, v in d.items():
+        d[k] = v/total
+
+    return d
+
+
+def expected_reward(state, action, state_value, request_probas, return_probas, n_free, n_max_storage):
+    """
+    Use to evaluate a single action in a single state.
+    :param state: tuple with (number of first location's cars, number of second location's cars)
+    :param action: int representing the number of cars to move from location 1 to 2.
+    :param state_value: state value matrix.
+    :param request_probas: dict with probabilities of request.
+    :param return_probas: dict with probabilities of return.
+    :param n_free: number of cars that one may move from one location to the other free of charge.
+    :param n_max_storage: number of cars allowed to expend the night free of charge.
+    :return: expected reward for the action in the given state.
+    """
+    # Execute the action
+    first_cars = min(state[0] - action, STOCK_LIMIT)
+    second_cars = min(state[1] + action, STOCK_LIMIT)
+    moved_cars = abs(state[0] - first_cars)
+    paid_moved_cars = max(moved_cars - n_free, 0)
+    reward = paid_moved_cars * -MOVE_COST
+
+    # Loop to get the value of each possibilities for rental
+    for rq1, rq1_proba in request_probas[0].items():
+        for rq2, rq2_proba in request_probas[1].items():
+            # Rentals in location one and two are independent
+            request_proba = rq1_proba * rq2_proba
+
+            # Rent reward
+            rented1 = min(rq1, first_cars)
+            rented2 = min(rq2, second_cars)
+            new_first_cars = first_cars - rented1
+            new_second_cars = second_cars - rented2
+            rent_reward = (rented1 + rented2) * RENTAL_PRICE
+
+            # Loop to get the value of each possible return
+            for rt1, rt1_proba in return_probas[0].items():
+                for rt2, rt2_proba in return_probas[1].items():
+                    # Returns in location one and two are independent
+                    return_proba = rt1_proba * rt2_proba
+
+                    # Update number of cars
+                    new_first_cars = min(new_first_cars + rt1, STOCK_LIMIT)
+                    new_second_cars = min(new_second_cars + rt2, STOCK_LIMIT)
+
+                    # Update storage cost
+                    storage_cost = 0
+                    if new_first_cars > n_max_storage:
+                        storage_cost += 4
+                    if new_second_cars > n_max_storage:
+                        storage_cost += 4
+
+                    # Update state reward
+                    reward += (request_proba * return_proba) * \
+                              (rent_reward + storage_cost +
+                               DISCOUNT_RATE * state_value[new_first_cars, new_second_cars])
+
+    return reward
+
+
+def optimise_rental(n_free=0, n_max_storage=None):
+    """
+    Optimise the rental problem.
+    :param n_free: number of cars that one may move from one location to the other free of charge.
+    :param n_max_storage: number of cars allowed to expend the night free of charge.
+    :return: policy and state value matrices.
+    """
+    state_value = np.zeros((STOCK_LIMIT+1, STOCK_LIMIT+1))
+    policy = np.zeros((STOCK_LIMIT+1, STOCK_LIMIT+1), dtype=np.int)
+    actions = [(-MOVE_LIMIT + i) for i in range(MOVE_LIMIT*2 + 1)]
+
+    # Calculate the poisson probabilities of each outcome for the store 0 and 1 and normalise their probabilities
+    request_probas = []
+    for l in REQUEST_LAMBDAS:
+        request_probas.append(get_poisson_dict(l, STOCK_LIMIT))
+    return_probas = []
+    for l in RETURN_LAMBDAS:
+        return_probas.append(get_poisson_dict(l, STOCK_LIMIT))
+
+    # Evaluate subsequently improved policies until no improvements can be done
+    improvement = True
+    iter = 0
+    while improvement:
+        iter += 1
+        improvement = False
+        state_change = TOLERANCE * 2
+        print('*--'*10)
+        print('Begining policy evaluation...')
+
+        # Evaluate the current policy
+        eval_iters = 0
+        while state_change > TOLERANCE:
+            old_state_value = state_value.copy()
+            for n_first, n_second in product(range(STOCK_LIMIT+1), range(STOCK_LIMIT+1)):
+                state_value[n_first, n_second] = expected_reward((n_first, n_second), policy[n_first, n_second],
+                                                                 state_value, request_probas, return_probas,
+                                                                 n_free, n_max_storage)
+            state_change = abs(old_state_value - state_value).max()
+            eval_iters += 1
+            print(state_change)
+        print('Policy evaluation finished!')
+
+        # Use new values to improve the policy
+        with tqdm(total=(STOCK_LIMIT+1)**2, desc="Improving policy") as pbar:
+            for n_first, n_second in product(range(STOCK_LIMIT + 1), range(STOCK_LIMIT + 1)):
+                actions_reward = []
+                for action in actions:
+                    # If we have enough cars to perform the action
+                    if (0 <= action <= n_first) or (-n_second <= action <= 0):
+                        actions_reward.append(expected_reward((n_first, n_second), action, state_value,
+                                                              request_probas, return_probas, n_free, n_max_storage))
+                    else:
+                        actions_reward.append(-np.inf)
+                pbar.update(1)
+
+                # Select best action
+                best_action = actions[np.argmax(actions_reward)]
+                if policy[n_first, n_second] != best_action:
+                    policy[n_first, n_second] = best_action
+                    improvement = True
+
+        print(f'\nIteration {iter} finished! Policy improved: {improvement}. Evaluation iters: {eval_iters}')
+
+    return policy, state_value
+
+
+if __name__ == '__main__':
+    n_free = 1
+    n_max_storage = 10
+    policy, state_value = optimise_rental(n_free, n_max_storage)
+
+    # Plot results
+    ax = sns.heatmap(policy, cmap='bwr', center=0, annot=True)
+    ax.invert_yaxis()
+    plt.title('Policy')
+    plt.ylabel('Cars in first location')
+    plt.xlabel('Cars in second location')
+    plt.savefig(f'graphs/jacks_move{MOVE_LIMIT}_limit{STOCK_LIMIT}_{n_free}_{n_max_storage}_policy.png')
+    plt.show()
+
+    ax = sns.heatmap(state_value, cmap='Blues')
+    ax.invert_yaxis()
+    plt.title('State Values')
+    plt.ylabel('Cars in first location')
+    plt.xlabel('Cars in second location')
+    plt.savefig(f'graphs/jacks_move{MOVE_LIMIT}_limit{STOCK_LIMIT}_{n_free}_{n_max_storage}_state_values.png')
+    plt.show()
